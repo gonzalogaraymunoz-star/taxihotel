e0930455b167e79a3b18dd9b94d1f7a9f5040f48
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-25 09:18:04 -0300

    Fix Elegir buttons and reconnect all vehicle CTAs to booking

--- app.js
+++ app.js
@@ -13,14 +13,7 @@ function chooseVehicleAndOpen(id){
 $$('[data-book-vehicle]').forEach(b=>b.onclick=()=>chooseVehicleAndOpen(b.dataset.bookVehicle));
 $('[data-frame-book]').forEach(b=>b.onclick=e=>{
   e.stopPropagation();
-  vehicle=b.dataset.frameBook;
-  const target=document.querySelector('#vehiculo-'+vehicle);
-  if(target){
-    history.replaceState(null,'','#vehiculo-'+vehicle);
-    target.scrollIntoView({behavior:'smooth',block:'start'});
-    target.classList.add('vehicle-focus');
-    setTimeout(()=>target.classList.remove('vehicle-focus'),1400);
-  }
+  chooseVehicleAndOpen(b.dataset.frameBook);
 });
 function openBooking(){booking.showModal();step=1;render()}
 
